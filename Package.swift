20b0d7752dedc51ef8e19b855c0d779208ed4822
Srđan Rašić <srdan.rasic@gmail.com>
2022-09-21 20:01:43 +0200

    Add macOS support

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "AsyncView",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v15), .macOS(.v10_15)],
     products: [
         .library(
             name: "AsyncView",
